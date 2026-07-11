067f5899e7f9f7afd914d2ff3c2d96a34da690ce
naveed <mohammednaveed01@gmail.com>
2026-07-11 15:58:41 +0530

    First Commit

---
+++ src/main/java/com/payit/common/entity/Auditable.java
@@ -0,0 +1,25 @@
+package com.payit.common.entity;
+
+import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class Auditable {
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String lastModfiedBy;
+}
